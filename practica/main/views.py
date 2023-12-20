4f00cb107aeaefa478f39f1204361b9c23461b30
Alejandrocg024 <alecamgal1@alum.us.es>
2023-12-20 02:25:43 +0100

    base

--- practica/main/views.py
+++ practica/main/views.py
@@ -1,8 +1,9 @@
 #encoding:utf-8
-from django.contrib.auth.models import User
-from django.shortcuts import render, get_object_or_404, HttpResponse
+from django.shortcuts import render
 
-#muestra una pantalla con el título del proyecto. Es un html estático
-def sobre(request):
-    html="<html><body>Proyecto de ejemplo de vistas</body></htm>"
-    return HttpResponse(html)
+
+def index(request):
+    return render(request, 'index.html')
+
+
+
--- practica/practica/urls.py
+++ practica/practica/urls.py
@@ -24,4 +24,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index),
 ]
